b71b1e42f8c46f3af5994c74732e690a4b97024f
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2018-04-03 18:21:29 +0200

    wip

---
+++ src/test/java/com/pillopl/messaging/card/infrastructure/ApplicationBaseClass.java
@@ -0,0 +1,44 @@
+package com.pillopl.messaging.card.infrastructure;
+
+import com.pillopl.messaging.card.model.CardApplicationRejected;
+import com.pillopl.messaging.card.model.CardGranted;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author Marcin Grzejszczak
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ApplicationBaseClass.Config.class)
+public class ApplicationBaseClass {
+	private static final String VALID_PERSONAL_ID = "86010197600";
+	private static final String INVALID_PERSONAL_ID = "86010156812";
+
+	@Autowired RabbitMqDomainEventPublisher publisher;
+
+	public void sendAccepted() {
+		publisher.publish(new CardGranted(UUID.randomUUID(), BigDecimal.ONE, VALID_PERSONAL_ID));
+	}
+
+	public void sendRejected() {
+		publisher.publish(new CardApplicationRejected(INVALID_PERSONAL_ID));
+	}
+
+	@Configuration
+	@EnableBinding(Source.class)
+	static class Config {
+		@Bean RabbitMqDomainEventPublisher publisher(Source source) {
+			return new RabbitMqDomainEventPublisher(source);
+		}
+	}
+}
